36fbe775745d19a843338c352586555f00354e4c
Logan Colborne <Logan.Colborne@gmail.com>
2020-04-25 17:13:53 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+  $STUDIO_NAME = "Logan Colborne";
+
+  $LEGAL_REPRESENTATIVE_NAME = "Logan Colborne";
+
+  $LEGAL_REPRESENTATIVE_TITLE = "Lead Developer";
+
+  $GAME_TITLES_SEPERATED_BY_COMMAS = "Fishing Game";	
+
+?>
